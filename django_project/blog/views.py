5031cf7b4f62e6d0ba75c2254f8e2ca9c69bf421
Ashok Shah <sa.ashokshah@gmail.com>
2019-06-10 06:50:47 -0400

    Changes in Admin Page and creating database , ORM

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,27 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 #from django.http import HttpResponse
 # Create your views here.
-
-posts = [
-    {
-        'author': 'Ashok',
-        'title': 'Blog Post',
-        'content': 'First post content',
-        'date_posted': 'June 9, 20219'
-    },
-    {
-        'author': 'Aditya',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'June 12, 20219'
-    }
-
-]
 
 
 def home (request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render (request, 'blog/home.html',context)
 
