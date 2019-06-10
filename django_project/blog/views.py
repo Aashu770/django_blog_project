daddc18ba4742066512ef946ea0c40f35e0dbcbe
Ashok Shah <sa.ashokshah@gmail.com>
2019-06-10 05:17:22 -0400

    Added Tempelates, Bootstrap and CSS

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Ashok',
+        'title': 'Blog Post',
+        'content': 'First post content',
+        'date_posted': 'June 9, 20219'
+    },
+    {
+        'author': 'Aditya',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 12, 20219'
+    }
+
+]
+
+
 def home (request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render (request, 'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render (request, 'blog/about.html',{'title': '@About'})
